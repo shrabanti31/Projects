bf41c403e69c64a7c6680ef032fbeb2ea99a6cb9
shrabanti31 <chowdhury.shrabanti@gmail.com>
2014-05-11 16:47:11 -0700

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,10 @@
+data=read.table("C:/Users/Shrabanti/Desktop/data.txt",sep=';')
+sub_metering1=data[,7]
+sub_metering2=data[,8]
+sub_metering3=data[,9]
+x=data[,2]
+plot(x,sub_metering1,col="black",type="l")
+points(x,sub_metering2,col="red",type="l")
+points(x,sub_metering3,col="blue",type="l")
+dev.copy(png,'C:/Users/Shrabanti/Desktop/Plot3.png',width=480,height=480)
+dev.off()
